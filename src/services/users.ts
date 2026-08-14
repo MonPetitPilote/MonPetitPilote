0661ba246a5d26ff683989786f86c9815d4549f5
Jordan Le Lay <jordanlelay@protonmail.com>
2026-08-14 21:32:00 +0200

    refactor: migrate services to ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -6,22 +6,25 @@ import {
   updateProfile,
 } from "firebase/auth";
 
-export async function createUser(email, password) {
+export async function createUser(email: string, password: string) {
   const auth = getAuth();
   return await createUserWithEmailAndPassword(auth, email, password);
 }
 
-export async function logIn(email, password) {
+export async function logIn(email: string, password: string) {
   const auth = getAuth();
   return await signInWithEmailAndPassword(auth, email, password);
 }
 
-export async function updateUserNickname(nickname) {
+export async function updateUserNickname(nickname: string) {
   const auth = getAuth();
+  if (!auth.currentUser) {
+    throw new Error("User must be logged in");
+  }
   return await updateProfile(auth.currentUser, { displayName: nickname });
 }
 
-export async function resetPassword(email) {
+export async function resetPassword(email: string) {
   const auth = getAuth();
   return await sendPasswordResetEmail(auth, email);
 }
